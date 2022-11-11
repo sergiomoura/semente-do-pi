ec3da0616cc152eb117aab8d0bdcbf03b632ddec
Sérgio Moura <smouracalmon@gmail.com>
2022-11-11 11:01:43 -0300

    Servidor configurado e criado

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Está config");
+// Importar o express
+const express = require('express');
+
+// Criar o servidor
+const servidor = express();
+
+// Definir a public como pasta de arquivos estáticos
+servidor.use(express.static("public"));
+
+// Definir as rotas
+servidor.get(
+    '/produtos',       // endereço ou caminho a receber a requisição
+    (req, res) => {    // função que será executada quando a req chegar
+        return res.sendFile(__dirname + '/views/produtos.html');
+    }
+)
+
+// Fazer meu servidor rodar
+servidor.listen(3000);
